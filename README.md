c62343907d9687dd6ecebd3483f4c38987bad25d
Vlerrie <roodt.brendan@gmail.com>
2022-05-05 16:28:34 +0200

    Authentication setup. Model, migration and controller creation

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
+## PROPAY Basic Crud Test
 
+### Requirements
+- Laravel 9
+- PHP 8.1
+- MySql
+
+### Setup
+First rename the ".env.example" file to ".env" and update following fields to match a database schema that was created
+<pre>
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE= #name of Schema
+DB_USERNAME=root
+DB_PASSWORD=
+</pre>
+
+When the database has been created and the fields above are correct please enter these commands within the project folder
+<pre>
+composer update
+php artisan migrate
+php artisan db:seed
+</pre>
---
+++ database/migrations/2022_05_05_140529_create_people_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('surname', 128);
+            $table->string('sa_id', 13);
+            $table->string('mobile', 13);
+            $table->string('email', 13);
+            $table->string('dob', 13);
+            $table->foreignId('language_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
